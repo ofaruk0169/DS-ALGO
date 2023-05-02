7509d3502f512be36944f943b36e83c0ce8c1e93
Omare Faruk <ofaruk0169@gmail.com>
2023-05-02 10:53:32 +0100

    freqPattern

---
+++ ColtSteeleCourse/freqCounterPattern.js
@@ -0,0 +1,10 @@
+// Challenge 1
+
+// Write a function called same, which accepts two arrays. The function should return true if every value in the array has
+// corresponding value squared int he second array. The frequency of values must be the same. 
+
+// examples
+
+same([1, 2, 3], [4, 1, 9]) //true 
+same([1,2,3], [1, 9]) //false 
+same([1,2,1], [4,4,1]) //false, must be the same frequency 
