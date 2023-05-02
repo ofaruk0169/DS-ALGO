6474fce9e96bbb5a3138cf36d7793c0804dd81cd
Omare Faruk <ofaruk0169@gmail.com>
2023-05-02 11:03:17 +0100

    Man, why can I not just sit here and do this, what is wrong with me

--- ColtSteeleCourse/freqCounterPattern.js
+++ ColtSteeleCourse/freqCounterPattern.js
@@ -8,3 +8,20 @@
 same([1, 2, 3], [4, 1, 9]) //true 
 same([1,2,3], [1, 9]) //false 
 same([1,2,1], [4,4,1]) //false, must be the same frequency 
+
+//Naive Solution
+
+function same (arr1, arr2) {
+    // we can tell immediately if the first array will work with the second based upon if the same number of array items are present. 
+    if(arr1.length !== arr2.length) {
+        return false;
+    }
+    for(let i = 0; i < arr1.length; i++) {
+        let correctIndex = arr2.indexOf(arr1[i] **2)
+        if(correctIndex === -1) {
+            return false;
+        }
+        arr2.splice(correctIndex,1)
+    }
+    return true;
+}
