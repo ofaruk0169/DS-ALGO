9e61606db4fbe91f5e18a3f597275e41fc6588e1
Omare Faruk <ofaruk0169@gmail.com>
2023-05-13 22:26:57 +0100

    test

--- ColtSteeleCourse/multiplePointers.js
+++ ColtSteeleCourse/multiplePointers.js
@@ -83,6 +83,7 @@ function countUniqueValues(array) {
 arr = [1,1,2,3,3,4,5,6,6,7]
 
   function countUniqueValues(arr) {
+    if(arr.length === 0) return 0;
     var i = 0;
     for(var j = 1; j < arr.length; j++) {
         if(arr[i] !== arr[j]) {
