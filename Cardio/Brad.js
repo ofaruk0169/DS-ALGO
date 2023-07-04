079aa60d0ae6bf943388cbe697e1149539c9b96b
Omare Faruk <ofaruk0169@gmail.com>
2023-07-04 20:43:51 +0100

    capitalise first word of the string

--- Cardio/Brad.js
+++ Cardio/Brad.js
@@ -19,3 +19,47 @@
 // }
 
 // console.log(reverseString("ted"))
+
+//How to reverse an integer. 
+
+// function reverseInteger(int) {
+//     //same as above. However. we need to turn it into a string first. 
+//     let reversedInteger = int.toString().split('').reverse().join('');
+//     // As we combine it is actually a string, so we use this final function with the return statement to return a number. 
+//     return parseInt(reversedInteger);
+// }
+
+//Capitalise the first Letter in each word of the string
+
+// function capitaliseLetters(str) {
+//     //make everything lowercase then split by the gap between words. This will return a 3 part
+//     //Array, like our example "I love Javascript" will be ["i", "love", "javascript"]
+//     const strArr = str.toLowerCase().split(' ');
+
+//     //next we need to loop through the array of words. 
+//     for (let i = 0; i < strArr.length; i++) {
+//         // this transforms the string in each array item. And gets the first letter and uppercases it.
+//         strArr[i] = strArr[i].substring(0, 1).toUpperCase() +
+//                     //then we add the rest of the array string value, substring(1) carries on from the previous
+//                     //substring in that it is, 1 onwards. e.g. we split javascript to get "j", now we are returning "avascript"                  
+//                     strArr[i].substring(1);
+//     }
+//     //we want to return the string as a string not an array so we join again.
+//     return strArr.join(' ');
+// }
+
+
+//function without comments
+
+// function capitaliseLetters(str) {
+
+//     const strArr = str.toLowerCase().split(' ');
+//     for (let i = 0; i < strArr.length; i++) {
+//         strArr[i] = strArr[i].substring(0, 1).toUpperCase() +
+//                     strArr[i].substring(1);
+//     }
+//     return strArr.join(' ');
+// }
+
+// console.log(capitaliseLetters("i love javascript"));
+
