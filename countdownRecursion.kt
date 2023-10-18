75a04efd55d6ae11ed10a8472e82ad0aa839c8a7
Omare Faruk <ofaruk0169@gmail.com>
2023-10-18 11:27:48 +0100

    Update countdownRecursion.kt

--- countdownRecursion.kt
+++ countdownRecursion.kt
@@ -10,3 +10,21 @@ fun countdown(num: Int) {
 fun main() {
     countdown(9)
 }
+
+
+/////alternative method JavaScript style and why KOtlin stlye doesn't translate the same/////
+
+fun countdown(num: Int) {
+    var mutableNum = num // Declare a mutable variable to store the decremented value
+    if (mutableNum <= 0) {
+        println("LIFTOFF")
+    } else {
+        println(mutableNum)
+        mutableNum--
+        countdown(mutableNum)
+    }
+}
+
+fun main() {
+    countdown(9)
+}
