604eaa64418e742851c923c97481dbeb3856244c
Omare Faruk <ofaruk0169@gmail.com>
2023-10-25 11:51:03 +0100

    Create productOfArrayRecursion.kt

---
+++ productOfArrayRecursion.kt
@@ -0,0 +1,29 @@
+/*Write a function called productOfArray which takes in an array of numbers and returns the product of
+ them all.*/
+
+//note to self use helper recursion.
+
+fun productOfArray(arrayValues: IntArray): Int {
+    
+   total = 0
+    
+   for (i in arrayValues) {
+   
+       if (arrayValues[i] == arrayValues.size -1) return 0
+       
+       
+       
+   }
+    
+   
+    
+   
+}
+
+fun main() {
+    val result = productOfArray((0)
+    println(result)
+}
+
+// productOfArray([1,2,3]) // 6
+// productOfArray([1,2,3,10]) // 60
